54f6adec565d97668278e648b47cb2f49521faec
SunHaotian14 <snyxyns@126.com>
2022-01-12 21:53:33 +0800

    Initial Commit

---
+++ Vibrato/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vibrato
+//
+//  Created by Haotian SUN on 2022/1/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vibrato/VibratoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibratoApp.swift
+//  Vibrato
+//
+//  Created by Haotian SUN on 2022/1/12.
+//
+
+import SwiftUI
+
+@main
+struct VibratoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
